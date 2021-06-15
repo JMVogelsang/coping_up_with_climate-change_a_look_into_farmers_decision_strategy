2021134d68caec571e02ff560b6602b1e6d476c8
JMVogelsang <johannes.vogelsang@uni-bonn.de>
2021-06-15 14:58:45 +0200

    input pathway

--- Coping up with climate change code.R
+++ Coping up with climate change code.R
@@ -45,7 +45,7 @@ input_estimates_2020_2029
 #### Impact pathway determining farmers success ####
 library(DiagrammeR)
 mermaid("graph BT
-        Y(Yield)-->I(Income); linkStyle 0 stroke:blue, stroke-width:2px
+        Y(Yield)-->I(Income); linkStyle 0 stroke:black, stroke-width:3px
         MP(Market price)-->I; linkStyle 1 stroke: blue, stroke-width:2px
         I-->FR(Final result); linkStyle 2 stroke: brown, stroke-width:3px
         CL(Cost labour)-->FR; linkStyle 3 stroke: brown, stroke-width:3px
@@ -57,28 +57,28 @@ mermaid("graph BT
         style CM fill:green
         style FR fill:red")
 
-mermaid("graph LR
-        PRE(Precipitation)-->ABI(Abiotic Factors); 
-        RAD(Radiation)-->ABI;                      
-        WIN(Wind)-->ABI;                           
-        TEM(Temperature)-->ABI;                    
-        SOI(Soil Quality)-->ABI;
-        ABI-->FAS(Farmers Success);
+mermaid("graph BT
+        PRE(Precipitation)-->ABI(Abiotic Factors);     linkStyle 0 stroke:black, stroke-width:3px
+        RAD(Radiation)-->ABI;                          linkStyle 1 stroke:black, stroke-width:3px
+        WIN(Wind)-->ABI;                               linkStyle 2 stroke:black, stroke-width:3px
+        TEM(Temperature)-->ABI;                        linkStyle 3 stroke:black, stroke-width:3px
+        SOI(Soil Quality)-->ABI;                       linkStyle 4 stroke:black, stroke-width:3px
+        ABI-->FAS(Farmers Success);                    linkStyle 5 stroke:black, stroke-width:4px
         
-        PES(Pests)-->BIO(Biotic Factors);
-        WEE(Weeds)-->BIO;
-        PAT(Pathogenes)-->BIO;
-        BIO-->FAS;
+        PES(Pests)-->BIO(Biotic Factors);              linkStyle 6 stroke:black, stroke-width:3px
+        WEE(Weeds)-->BIO;                              linkStyle 7 stroke:black, stroke-width:3px
+        PAT(Pathogenes)-->BIO;                         linkStyle 8 stroke:black, stroke-width:3px
+        BIO-->FAS;                                     linkStyle 9 stroke:black, stroke-width:4px
         
-        RIP(Rice Price)-->SEF(Socio Economic Factors);
-        LAC(Labour Cost)-->SEF;
-        LAA(Labour Availability)-->SEF;
-        IRC(Irrigation Cost)-->SEF;
-        FEC(Fertilizer Cost)-->SEF;
-        PEC(Pesticide Cost)-->SEF;
-        MAC(Machinery Cost)-->SEF;
-        RIY(Rice Yield)-->SEF;
-        SEF-->FAS;
+        RIP(Rice Price)-->SEF(Socio Economic Factors); linkStyle 10 stroke:black, stroke-width:3px
+        LAC(Labour Cost)-->SEF;                        linkStyle 11 stroke:black, stroke-width:3px
+        LAA(Labour Availability)-->SEF;                linkStyle 12 stroke:black, stroke-width:3px
+        IRC(Irrigation Cost)-->SEF;                    linkStyle 13 stroke:black, stroke-width:3px
+        FEC(Fertilizer Cost)-->SEF;                    linkStyle 14 stroke:black, stroke-width:3px
+        PEC(Pesticide Cost)-->SEF;                     linkStyle 15 stroke:black, stroke-width:3px
+        MAC(Machinery Cost)-->SEF;                     linkStyle 16 stroke:black, stroke-width:3px
+        RIY(Rice Yield)-->SEF;                         linkStyle 17 stroke:black, stroke-width:3px
+        SEF-->FAS;                                     linkStyle 18 stroke:black, stroke-width:4px
         
         style PRE fill:orange
         style RAD fill:orange
