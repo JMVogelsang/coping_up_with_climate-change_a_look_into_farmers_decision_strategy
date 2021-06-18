a3324b123f3de4cf4741d6d5aeb81c7cc5ab75f7
JMVogelsang <84016747+JMVogelsang@users.noreply.github.com>
2021-06-18 21:21:24 +0200

    Basic rice model
    First try to build a more less complete model. However, some variables are still missing and it is VERY basic.

--- Coping up with climate change code.R
+++ Coping up with climate change code.R
@@ -1,95 +1,68 @@
 #### Input table for values in time period 1990 to 1999 ####
-input_estimates_1990_1999 <- data.frame(variable = c("Precipitation", "Temperature"),
-                                        lower = c(2235.904, 22.76),
+input_estimates_1990_1999 <- data.frame(variable = c("Precipitation", "Radiation", "Wind", "Temperature", "Soil_quality",
+                                                     "Pests", "Weeds", "Pathogenes", "Rice_price", "Labour_cost", "Labour_availability",
+                                                     "Irrigation_cost", "Fertilizer_cost", "Pesticide_cost", "Machinery_cost", "Rice_yield", "var_CV"),
+                                        lower = c(1940.6, 44.16, 5, 22.76, 70, 10, 5, 5, 0.5, 50, 1, 40, 20, 10, 50, 2000, 50),
                                         median = NA,
-                                        upper = c(2235.904, 30.73),
-                                        distribution = c("const", "posnorm"),
-                                        label = c("Average precipitation (mm/year)", "Average temperature (?C)"),
-                                        Description = c("10 year average annual precipitation",
-                                                        "10 year average anual temperature"))
+                                        upper = c(2972.2, 95.66, 30, 30.73, 90, 20, 10, 15, 0.7, 70, 5, 50, 30, 20, 90, 4000, 50),
+                                        distribution = c("posnorm", "posnorm", "posnorm", "posnorm", "posnorm", "posnorm", "posnorm", "posnorm",
+                                                         "posnorm", "posnorm", "posnorm", "posnorm", "posnorm", "posnorm", "posnorm", "posnorm", 
+                                                         "const"),
+                                        label = c("mm/year", "MJ/m^2", "% yield loss", "°C", "SQI", "% yield loss", "% yield loss", "% yield loss",
+                                                  "USD/kg", "USD/ha", "days until labour", "USD/ha", "USD/ha", "USD/ha", "USD/ha", "kg/ha",
+                                                  "Coefficient of variation"),
+                                        Description = c("Annual precipitation sum", "Average annual radiation", "Yield loss due to heavy wind",
+                                                        "Average annual temperature", "Soil quality index", "Yield loss due to pest infestation",
+                                                        "Yield loss due to weed infestation", "Yield los due to pathogene infestation", 
+                                                        "Rice market price", "Labour market cost", "Days to find workers", "Price of irrigation",
+                                                        "Price of fertilizer", "Price of pesticide", "Price of machinery", "Rice yield", 
+                                                        "Coefficient of variation (measure of relative variability)"))
 input_estimates_1990_1999
 
-#### Input table for values in time period 2000 to 2009 ####
-input_estimates_2000_2009 <- data.frame(variable = c("Precipitation", "Temperature"),
-                                        lower = c(2597.399, 22.94),
-                                        median = NA,
-                                        upper = c(2597.399, 30.42),
-                                        distribution = c("const", "posnorm"),
-                                        label = c("Average precipitation (mm/year)", "Average temperature (?C)"),
-                                        Description = c("10 year average annual precipitation",
-                                                        "10 year average anual temperature"))
-input_estimates_2000_2009
+####  Model function ####
+library(decisionSupport)
+rice_function <- function(){
+  # yield losses dependent on % yield loss due to wind, pests, weeds and pathogens.
+  yield_loss <- Wind + Pests + Weeds + Pathogenes
+  # income dependent rice price times rice yield. Rice yield is reduced about yield loss.
+  income <- Rice_price * (Rice_yield * ((100 - yield_loss)/100))
+  # overall cost as sum of labour, irrigation, fertilizer, pesticide and machinery cost
+  overall_costs <- Labour_cost + Irrigation_cost + Fertilizer_cost + Pesticide_cost + Machinery_cost
+  # final result is income subtracted by all costs
+  final_result <- income - overall_costs
+  
+  return(list(final_result = final_result))
+  
+}
 
-#### Input table for values in time period 2010 to 2019 ####
-input_estimates_2010_2019 <- data.frame(variable = c("Precipitation", "Temperature"),
-                                        lower = c(2698.525, 23.13),
-                                        median = NA,
-                                        upper = c(2698.525, 31.01),
-                                        distribution = c("const", "posnorm"),
-                                        label = c("Average precipitation (mm/year)", "Average temperature (?C)"),
-                                        Description = c("10 year average annual precipitation",
-                                                        "10 year average anual temperature"))
-input_estimates_2010_2019
+library(decisionSupport)
+# Run the Monte Carlo simulation using the model function and data from input_estimates.
+rice_mc_simulation <- mcSimulation(estimate = as.estimate(input_estimates_1990_1999),
+                                    model_function = rice_function,
+                                    numberOfModelRuns = 1000,
+                                    functionSyntax = "plainNames")
 
-#### Input table for values in time period 2020 to 2029 ####
-input_estimates_2020_2029 <- data.frame(variable = c("Precipitation", "Temperature"),
-                                        lower = c(0, 0),
-                                        median = NA,
-                                        upper = c(0, 0),
-                                        distribution = c("const", "posnorm"),
-                                        label = c("Average precipitation (mm/year)", "Average temperature (?C)"),
-                                        Description = c("10 year average annual precipitation",
-                                                        "10 year average anual temperature"))
-input_estimates_2020_2029
+rice_mc_simulation
 
-#### Impact pathway determining farmers success ####
-library(DiagrammeR)
-mermaid("graph BT
-        PRE(Precipitation)-->ABI(Abiotic Factors);     linkStyle 0 stroke:black, stroke-width:3px
-        RAD(Radiation)-->ABI;                          linkStyle 1 stroke:black, stroke-width:3px
-        WIN(Wind)-->ABI;                               linkStyle 2 stroke:black, stroke-width:3px
-        TEM(Temperature)-->ABI;                        linkStyle 3 stroke:black, stroke-width:3px
-        SOI(Soil Quality)-->ABI;                       linkStyle 4 stroke:black, stroke-width:3px
-        ABI-->FAS(Farmers Success);                    linkStyle 5 stroke:black, stroke-width:4px
-        
-        PES(Pests)-->BIO(Biotic Factors);              linkStyle 6 stroke:black, stroke-width:3px
-        WEE(Weeds)-->BIO;                              linkStyle 7 stroke:black, stroke-width:3px
-        PAT(Pathogenes)-->BIO;                         linkStyle 8 stroke:black, stroke-width:3px
-        BIO-->FAS;                                     linkStyle 9 stroke:black, stroke-width:4px
-        
-        RIP(Rice Price)-->SEF(Socio Economic Factors); linkStyle 10 stroke:black, stroke-width:3px
-        LAC(Labour Cost)-->SEF;                        linkStyle 11 stroke:black, stroke-width:3px
-        LAA(Labour Availability)-->SEF;                linkStyle 12 stroke:black, stroke-width:3px
-        IRC(Irrigation Cost)-->SEF;                    linkStyle 13 stroke:black, stroke-width:3px
-        FEC(Fertilizer Cost)-->SEF;                    linkStyle 14 stroke:black, stroke-width:3px
-        PEC(Pesticide Cost)-->SEF;                     linkStyle 15 stroke:black, stroke-width:3px
-        MAC(Machinery Cost)-->SEF;                     linkStyle 16 stroke:black, stroke-width:3px
-        RIY(Rice Yield)-->SEF;                         linkStyle 17 stroke:black, stroke-width:3px
-        SEF-->FAS;                                     linkStyle 18 stroke:black, stroke-width:4px
-        
-        style PRE fill:orange
-        style RAD fill:orange
-        style WIN fill:orange
-        style TEM fill:orange
-        style SOI fill:orange
-        style ABI fill:blue
-        style PES fill:green
-        style WEE fill:green
-        style PAT fill:green
-        style BIO fill:blue
-        style RIP fill:yellow
-        style LAC fill:yellow
-        style LAA fill:yellow
-        style IRC fill:yellow
-        style FEC fill:yellow
-        style PEC fill:yellow
-        style MAC fill:yellow
-        style RIY fill:yellow
-        style SEF fill:blue
-        style FAS fill:red")
-        
-<<<<<<< HEAD
-#### Implementing model function ####
-=======
-#### Implementing model function ####
->>>>>>> 4bf6d39ced3949d3f0367fc1cab7f510529fcc0f
+
+
+# Visualize model output as graph.  
+plot_distributions(mcSimulation_object = rice_mc_simulation,
+                   vars = "final_result",
+                   method = "boxplot_density",
+                   old_names = "final_result",
+                   new_names = "Outcome distribution for profits in $/ha in decade 1990 - 1999.")
+
+# Visualize model output as different graph. 
+plot_distributions(mcSimulation_object = rice_mc_simulation,
+                   vars = "final_result",
+                   method = "hist_simple_overlay",
+                   old_names = "final_result",
+                   new_names = "Outcome distribution for profits")
+
+# Visualize model output as boxplot.. 
+plot_distributions(mcSimulation_object = rice_mc_simulation,
+                   vars = "final_result",
+                   method = "boxplot",
+                   old_names = "final_result",
+                   new_names = "Outcome distribution for profits")
